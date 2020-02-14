13f03a78aa7e64cc584e89d601f2603dee575931
Sagheer Hussain <sagheerhzardari@gmail.com>
2020-02-14 17:20:00 +0500

    commit 2

--- app/src/main/java/com/sagheer/javedlib/MainActivity.kt
+++ app/src/main/java/com/sagheer/javedlib/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.sagheer.javedlib
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.sagheer.javedlibaray.MyBasicFunctions
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        var ans = MyBasicFunctions().add(40, 50)
+        Toast.makeText(this, ans, Toast.LENGTH_SHORT).show()
+
     }
 }
--- javedlibaray/src/main/java/com/sagheer/javedlibaray/MyBasicFunctions.kt
+++ javedlibaray/src/main/java/com/sagheer/javedlibaray/MyBasicFunctions.kt
@@ -6,4 +6,8 @@ class MyBasicFunctions {
     fun add(num1: Int, num2: Int): String{
         return (num1+num2).toString()
     }
+
+    fun sub(num1: Int, num2: Int): String {
+        return (num1 - num2).toString()
+    }
 }
